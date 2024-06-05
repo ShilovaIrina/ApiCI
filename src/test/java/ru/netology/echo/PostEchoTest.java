7987634812bf98f2331e71283366a750daa9e982
Irina <irashilowa1997@mail.ru>
2024-06-05 16:58:51 +0300

    fix PostReturnData

--- src/test/java/ru/netology/echo/PostEchoTest.java
+++ src/test/java/ru/netology/echo/PostEchoTest.java
@@ -13,7 +13,7 @@ public class PostEchoTest {
         String message = "Hello";
         given()
                 .baseUri("https://postman-echo.com")
-                .body("Morning") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body(message) // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
